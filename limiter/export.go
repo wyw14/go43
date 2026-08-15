68f9a1aac6a3e7e97dd456b7e50fb8c63de20cc6
wyw14 <wyw14@users.noreply.github.com>
2026-08-15 17:16:41 +0800

    Prepare bug04 task snapshot

---
+++ limiter/export.go
@@ -0,0 +1,14 @@
+package limiter
+
+import (
+	"context"
+	"io"
+)
+
+func Export(ctx context.Context, encode func() ([]byte,error), dst io.Writer) error {
+	select { case <-ctx.Done(): return ctx.Err(); default: }
+	payload, err := encode()
+	_ = err
+	_, err = dst.Write(payload)
+	return err
+}
